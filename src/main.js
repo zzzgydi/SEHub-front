b3a180825963d6392db7db17014fac68d795963d
SuperGuangDi <547579053@qq.com>
2019-02-18 17:52:42 +0800

    init all

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
