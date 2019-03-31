94de88af2970ef1b833eedcade16ab8f9be37e88
SuperGuangDi <547579053@qq.com>
2019-03-31 17:43:03 +0800

    save

--- src/views/others/Others.vue
+++ src/views/others/Others.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="default-container">
     <simple-header title="其他"></simple-header>
+    <div class="module-container">
+      <div class="module-list-box">
+        <div class="module-regsys-box">
+          <div class="regsys-animation-box">
+            <div class="ra--book">
+              <div class="ra-book-word"></div>
+              <div class="ra-book-word2"></div>
+            </div>
+            <div class="ra--pencil">
+              <div class="ra-pen-nib"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -14,6 +29,5 @@ export default {
   }
 }
 </script>
-<style lang="stylus" scoped>
-@import '../../assets/css/default';
+<style lang="stylus" scoped src="./others.styl">
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  assetsDir: 'dist/static'
+  assetsDir: 'static'
 }
